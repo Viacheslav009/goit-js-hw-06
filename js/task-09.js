112604f96186e14bf2579a026e3a2e2c465bbf13
Viacheslav009 <mstislavpushkov@gmail.com>
2023-02-20 19:47:02 +0200

    fix hw 09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const hex = document.querySelector('.color');
+const buttonChangeColor = document.querySelector('.change-color');
+buttonChangeColor.addEventListener('click', getRandomHexColorText);
+
+function getRandomHexColorText() {
+  document.body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  hex.textContent = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
