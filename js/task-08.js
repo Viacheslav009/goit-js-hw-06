d48efa7b4add86937bed7ef2ca62e549c16868ec
Viacheslav009 <mstislavpushkov@gmail.com>
2023-02-22 21:44:10 +0200

    all hw fix

--- js/task-08.js
+++ js/task-08.js
@@ -3,14 +3,23 @@ form.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(event) {
   event.preventDefault();
-  const {
-    elements: { email, password },
-  } = event.currentTarget;
+  // const {
+  //   elements: { email, password },
+  // } = event.currentTarget;
 
-  if (email.value === '' || password.value === '') {
+  // if (email.value === '' || password.value === '') {
+  //   return alert('Please fill in all the fields!');
+  // }
+  const formElements = event.currentTarget.elements;
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+
+  if (email === '' || password === '') {
     return alert('Please fill in all the fields!');
   }
 
-  console.log(`email: ${email.value}, Password: ${password.value}`);
+  const formData = { email, password };
+  // console.log(`email: ${email.value}, Password: ${password.value}`);
+  console.log(formData);
   event.currentTarget.reset();
 }
