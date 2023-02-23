1d0fa4f0b4028a454f993198d76253cb996a1139
Viacheslav009 <mstislavpushkov@gmail.com>
2023-02-23 23:23:51 +0200

    fix all

--- js/task-05.js
+++ js/task-05.js
@@ -3,7 +3,7 @@ const output = document.querySelector('#name-output');
 
 textInput.addEventListener('input', (event) => {
   if (event.currentTarget.value.trim() === '') {
-    output.textContent === 'Anonymous';
+    output.textContent = 'Anonymous';
   } else {
     output.textContent = event.currentTarget.value;
   }
