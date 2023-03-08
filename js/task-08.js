642fa746d9e574e08f1926fc39c5cc962ec26ee2
Viacheslav009 <mstislavpushkov@gmail.com>
2023-03-08 12:11:43 +0200

    error

--- js/task-08.js
+++ js/task-08.js
@@ -13,7 +13,6 @@ function onFormSubmit(event) {
   const formElements = event.currentTarget.elements;
   const email = formElements.email.value;
   const password = formElements.password.value;
-
   if (email === '' || password === '') {
     return alert('Please fill in all the fields!');
   }
